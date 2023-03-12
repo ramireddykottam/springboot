ffce057379c9a79b84e87fec9df5208be5cc4085
ramireddy <ramireddyk90@gmail.com>
2023-03-12 10:35:46 +0000

    solution

---
+++ src/main/java/com/example/goodreads/service/BookH2Service.java
@@ -0,0 +1,10 @@
+package com.example.goodreads.service;
+import java.util.*;
+import com.example.goodreads.repository.BookRepository;
+import com.example.goodreads.model.Book;
+public class BookH2Services implements BookRepository{
+    @Override
+    public ArrayList<Book> getBooks(){
+        
+    }
+}
